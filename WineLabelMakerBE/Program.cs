ee55effe171d41a78b99f0bc0913da3f616d7c80
MREBECCA98 <reb.matarozzo@gmail.com>
2026-05-27 16:30:15 +0200

    modify

--- WineLabelMakerBE/Program.cs
+++ WineLabelMakerBE/Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //DBCONTEXT CONFIGURATION WITH SQL SERVER
 builder.Services.AddDbContext<ApplicationDbContext>(
-    option => option.UseNpgsql(
+    option => option.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"))
     );
 
