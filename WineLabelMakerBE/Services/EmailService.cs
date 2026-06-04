cd5fe0a249c5bf7c4f0ae8c8ba66ec9923c1a21d
MREBECCA98 <reb.matarozzo@gmail.com>
2026-06-04 15:22:19 +0200

    Modify EmailWithLabelAsync

--- WineLabelMakerBE/Services/EmailService.cs
+++ WineLabelMakerBE/Services/EmailService.cs
@@ -34,8 +34,35 @@ namespace WineLabelMakerBE.Services
             message.Subject = subject;
             message.TextBody = body;
 
+            byte[] fileBytes = await File.ReadAllBytesAsync(imagePath);
+            string fileName = Path.GetFileName(imagePath);
+
+            message.Attachments = new List<Resend.EmailAttachment>
+    {
+        new Resend.EmailAttachment
+        {
+            Filename = fileName,
+            Content = Convert.ToBase64String(fileBytes),
+            ContentType = "image/png"
+        }
+    };
+
             var response = await _resend.EmailSendAsync(message);
+
             return response != null;
         }
+        //public async Task<bool> EmailWithLabelAsync(string toEmail, string subject, string body, string imagePath)
+        //{
+        //    if (!File.Exists(imagePath)) return false;
+
+        //    var message = new EmailMessage();
+        //    message.From = "Wine Label Maker <noreply@winelabelmaker.com>";
+        //    message.To.Add(toEmail);
+        //    message.Subject = subject;
+        //    message.TextBody = body;
+
+        //    var response = await _resend.EmailSendAsync(message);
+        //    return response != null;
+        //}
     }
 }
