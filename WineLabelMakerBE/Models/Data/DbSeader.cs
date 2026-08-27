70de4f0323b5af7721ec657b92b66d0b53be2916
MREBECCA98 <reb.matarozzo@gmail.com>
2026-08-27 09:56:10 +0200

    modify dbSeader

--- WineLabelMakerBE/Models/Data/DbSeader.cs
+++ WineLabelMakerBE/Models/Data/DbSeader.cs
@@ -1,54 +1,10 @@
-﻿//using Microsoft.AspNetCore.Identity;
-//using WineLabelMakerBE.Models.Entity;
+﻿using Microsoft.AspNetCore.Identity;
+using WineLabelMakerBE.Models.Entity;
 
 ////Using the DbSeader class to create the admin
 ////DbSeeder is used to automatically create initial data in the database when the app starts
 ////It works using Identity Services and is executed at app startup via Program.cs
 ////Sensitive data in appsettings.Development.json - gitignore
-
-//namespace WineLabelMakerBE.Models.Data
-//{
-//    public static class DbSeader
-//    {
-//        public static async Task SeedAsync(IServiceProvider services)
-//        {
-//            using var scope = services.CreateScope();
-
-//            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-//            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-//            var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
-
-//            var adminSection = configuration.GetSection("Seeder:Admin");
-
-//            var email = adminSection["Email"];
-//            var password = adminSection["Password"];
-//            var user = await userManager.FindByEmailAsync(email);
-
-//            if (user == null)
-//            {
-//                user = new ApplicationUser
-//                {
-//                    Name = adminSection["Name"],
-//                    Surname = adminSection["Surname"],
-//                    CompanyName = adminSection["CompanyName"],
-//                    UserName = email,
-//                    Email = email,
-//                    EmailConfirmed = true,
-//                    CreatedAt = DateTime.UtcNow,
-//                    IsDeleted = false
-//                };
-
-//                await userManager.CreateAsync(user, password);
-//                await userManager.AddToRoleAsync(user, "Admin");
-//            }
-//        }
-//    }
-
-//}
-
-using Microsoft.AspNetCore.Identity;
-using WineLabelMakerBE.Models.Entity;
 
 namespace WineLabelMakerBE.Models.Data
 {
@@ -60,18 +16,17 @@ namespace WineLabelMakerBE.Models.Data
 
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
             var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
             var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
-
-            var adminSection = configuration.GetSection("Seeder:Admin");
-
-            var email = adminSection["Email"];
-            var password = adminSection["Password"];
-
 
             if (!await roleManager.RoleExistsAsync("Admin"))
             {
                 await roleManager.CreateAsync(new IdentityRole("Admin"));
             }
+
+            var adminSection = configuration.GetSection("Seeder:Admin");
+            var email = adminSection["Email"];
+            var password = adminSection["Password"];
 
             var user = await userManager.FindByEmailAsync(email);
 
@@ -89,15 +44,17 @@ namespace WineLabelMakerBE.Models.Data
                     IsDeleted = false
                 };
 
-                var createResult = await userManager.CreateAsync(user, password);
+                await userManager.CreateAsync(user, password);
 
-
-                if (createResult.Succeeded)
+                if (!await userManager.IsInRoleAsync(user, "Admin"))
                 {
                     await userManager.AddToRoleAsync(user, "Admin");
                 }
+
             }
         }
     }
+
 }
+
 
